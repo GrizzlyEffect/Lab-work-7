3fb8884bc066a40f545191b704b2b1b92d6af8f0
Vlad Saiapin <vsayapin95@gmail.com>
2024-11-06 19:15:17 +0200

    Add project files.

---
+++ Laba 7/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_7
+{
+    public class Person : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public double Weight { get; set; }
+        public double Height { get; set; }
+        public bool HasAuto {  get; set; }
+        public bool HasBike { get; set; }
+        public double BMI { get; set; }
+
+
+        public double CalculateBMI()
+        {
+            return Weight / (Height * Height);
+        }
+
+
+        public Person()
+        {
+
+        }
+
+        public Person(string firstname, string lastname, string gender,
+                      int age, double weight, double height, bool hasAuto, bool hasBike, double calculateBMI)
+        {
+            FirstName = firstname;
+            LastName = lastname;
+            Gender = gender;
+            Age = age;
+            Weight = weight;
+            Height = height;
+            HasAuto = hasAuto;
+            HasBike = hasBike;
+            BMI = calculateBMI;
+        }
+
+        public string Info()
+        {
+            return FirstName + ", " + LastName + ", " + Gender + ", " + Age;
+        }
+
+        public int CompareTo(object obj)
+        {
+            Person p = obj as Person;
+            return string.Compare(this.FirstName, p.FirstName);
+        }
+    }
+}
---
+++ Laba 7/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laba_7
+{
+    internal class Program
+    {
+
+        static List<Person> persons;
+
+        static void PrintPersons()
+        {
+            foreach (Person person in persons)
+            {
+                Console.WriteLine($"{person.FirstName}\t{person.LastName}\t{person.Gender}\t{person.Age}");
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            persons = new List<Person>();
+
+            if (!File.Exists("t1.persons"))
+            {
+                Console.WriteLine("Файл 't1.persons' не знайдено.");
+                return;
+            }
+
+            using (FileStream fs = new FileStream("t1.persons", FileMode.Open))
+            using (BinaryReader reader = new BinaryReader(fs))
+            {
+                try
+                {
+                    Console.WriteLine("      Читаємо дані з файлу...\n");
+
+                    while (reader.BaseStream.Position < reader.BaseStream.Length)
+                    {
+                        try
+                        {
+                            Person person = new Person
+                            {
+                                FirstName = reader.ReadString(),
+                                LastName = reader.ReadString(),
+                                Gender = reader.ReadString(),
+                                Age = reader.ReadInt32(),
+                                Height = reader.ReadDouble(),
+                                Weight = reader.ReadDouble(),
+                                HasAuto = reader.ReadBoolean(),
+                                HasBike = reader.ReadBoolean(),
+                                BMI = reader.ReadDouble()
+                            };
+
+                            person.BMI = person.CalculateBMI();
+                            persons.Add(person);
+                        }
+                        catch (EndOfStreamException)
+                        {
+                            Console.WriteLine("      Досягнуто кінця файлу при спробі прочитати об'єкт.");
+                            break;
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine("      Помилка під час читання об'єкта: {0}", ex.Message);
+                            break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("      Сталась помилка: {0}", ex.Message);
+                }
+            }
+            Console.WriteLine("      Несортований перелік персон: {0}", persons.Count);
+            PrintPersons();
+            persons.Sort();
+            Console.WriteLine("      Сортований перелік персон: {0}", persons.Count);
+            PrintPersons();
+            Console.WriteLine("      Додаємо новий запис: Mark");
+            Person personVlad = new Person("Mark", "Markov", "Male", 22, 1.85, 69, false, false, 22.2);
+            persons.Add(personVlad);
+            persons.Sort();
+            Console.WriteLine("      Перелік персон: {0}", persons.Count);
+            PrintPersons();
+            Console.WriteLine("      Видаляємо останнє значення");
+            persons.RemoveAt(persons.Count - 1);
+            Console.WriteLine("      Перелік персон: {0}", persons.Count);
+            PrintPersons();
+        }
+    }
+}
